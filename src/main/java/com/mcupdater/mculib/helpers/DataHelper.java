f5c1677f1280d9b63f03b38d1284acee3943e25e
Scott M. Barbour <smbarbour@gmail.com>
2024-06-12 01:19:24 -0500

    Add functionality to support fluids and machine data in recipes

--- src/main/java/com/mcupdater/mculib/helpers/DataHelper.java
+++ src/main/java/com/mcupdater/mculib/helpers/DataHelper.java
@@ -1,13 +1,22 @@
 package com.mcupdater.mculib.helpers;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.data.BuiltinRegistries;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.GsonHelper;
 import net.minecraft.world.MenuProvider;
 import net.minecraft.world.Nameable;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -49,5 +58,17 @@ public class DataHelper {
             adjacentNames.put(side,name);
         }
         return adjacentNames;
+    }
+
+    public static FluidStack getJsonFluidStack(JsonObject json) {
+        if (json.has("fluid") && json.has("fluidAmount")) {
+            String fluidName = GsonHelper.getAsString(json, "fluid");
+            ResourceLocation resourceLocation = new ResourceLocation(fluidName);
+            Fluid fluid = ForgeRegistries.FLUIDS.getValue(resourceLocation);
+            int fluidAmount = GsonHelper.getAsInt(json, "fluidAmount");
+            return new FluidStack(fluid, fluidAmount);
+        } else {
+            return FluidStack.EMPTY;
+        }
     }
 }
---
+++ src/main/java/com/mcupdater/mculib/inventory/MachineContainer.java
@@ -0,0 +1,71 @@
+package com.mcupdater.mculib.inventory;
+
+import com.mcupdater.mculib.block.AbstractMachineBlockEntity;
+import com.mcupdater.mculib.capabilities.EnergyResourceHandler;
+import net.minecraft.world.Container;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.fluids.capability.IFluidHandler;
+
+public class MachineContainer implements Container {
+
+
+	private final AbstractMachineBlockEntity machine;
+
+	public MachineContainer(AbstractMachineBlockEntity entity) {
+		this.machine = entity;
+	}
+
+	public IFluidHandler getFluidHandler() {
+		return machine.getFluidHandler();
+	}
+
+	public EnergyResourceHandler getEnergyStorage() {
+		return machine.getEnergyStorage();
+	}
+
+	@Override
+	public int getContainerSize() {
+		return machine.getInventory().getContainerSize();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return machine.getInventory().isEmpty();
+	}
+
+	@Override
+	public ItemStack getItem(int pSlot) {
+		return machine.getInventory().getItem(pSlot);
+	}
+
+	@Override
+	public ItemStack removeItem(int pSlot, int pAmount) {
+		return machine.getInventory().removeItem(pSlot, pAmount);
+	}
+
+	@Override
+	public ItemStack removeItemNoUpdate(int pSlot) {
+		return machine.getInventory().removeItemNoUpdate(pSlot);
+	}
+
+	@Override
+	public void setItem(int pSlot, ItemStack pStack) {
+		machine.getInventory().setItem(pSlot, pStack);
+	}
+
+	@Override
+	public void setChanged() {
+		machine.getInventory().setChanged();
+	}
+
+	@Override
+	public boolean stillValid(Player pPlayer) {
+		return machine.getInventory().stillValid(pPlayer);
+	}
+
+	@Override
+	public void clearContent() {
+		machine.getInventory().clearContent();
+	}
+}
